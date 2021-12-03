be11d21c025954dd799aca4a3eb1b93d9b10b43d
Troels Mortensen <tromortensen@hotmail.com>
2021-12-03 12:04:54 +0100

    Initial commit

---
+++ Domain/Models/Todo.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.Models;
+
+public class Todo
+{
+    public int Id { get; set; }
+    public int OwnerId { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public bool IsCompleted { get; set; }
+
+    public Todo(int ownerId, string title, string description)
+    {
+        OwnerId = ownerId;
+        Title = title;
+        Description = description;
+    }
+}
